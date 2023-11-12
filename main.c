338a10ff515798b7f1c42bc882cd897c8f78d985
muegan <2216869@sookmyung.ac.kr>
2023-11-12 22:48:21 +0900

    5번

--- main.c
+++ main.c
@@ -4,8 +4,26 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(void){
-	char str[30] = "happy C programming";
+int main(void){
 	
-	printf("length of \"%s\" string : %i", str, strlen(str));
+	char word1[30], word2[30], word3[30];
+	
+	printf("input a word: ");
+	scanf("%s", word1);
+	
+	printf("input a word: ");
+	scanf("%s", word2);
+	
+	printf("input a word: ");
+	scanf("%s", word3);
+	
+	
+	FILE *fp;
+	fp = fopen("sample.txt", "w");
+
+	fprintf(fp, "%s\n%s\n%s\n", word1, word2, word3);
+		
+	fclose(fp);
+	
+	return 0;
 }
